5766f97c56d56fdad436bb7c67fd328677d5e19e
Joël Franusic <joel@franusic.com>
2010-01-12 22:43:04 -0800

    Move classes to seperate files. Rename JSONDNSError class to DnsrubyError

--- jsondnsd.rb
+++ jsondnsd.rb
@@ -9,56 +9,10 @@ require 'yajl'
 require 'pp'
 include Dnsruby
 
-class Hash
-  # Used by to_dnsruby_message to convert a Hash into a new Dnsruby::Message
-  def to_rdata_string   # this should actually be a monkeypatch to Dnsruby::RR "new_from_json_hash"
-    throw SyntaxError unless defined? self[:name] and defined? self[:ttl] and defined? self[:class] and defined? self[:rdata]
-    if self[:rdata].class == Array
-      rdata = self[:rdata].join(' ') 
-    else
-      rdata = self[:rdata]
-    end
-    "%s %s %s %s %s" % [self[:name],self[:ttl],self[:class],self[:type],rdata]
-  end
-
-  # Given a hash (but should be a string), create a Dnsruby::Message
-  def to_dnsruby_message # this should actually be a monkeypatch to Dnsruby::Message "new_from_json"?
-    throw SyntaxError unless defined? self[:header] and defined? self[:question] and defined? self[:answer] and defined? self[:authority]
-    msg = Message.new
-    header = Header.new
-    self[:header].each do |k,v|
-      key = "@#{k}"
-      next unless header.instance_variable_defined?(key)
-      if k == :opcode
-        header.opcode = v
-      elsif k == :rcode
-        header.rcode = v
-      else
-        header.instance_variable_set(key,v)
-      end
-    end
-    msg.header = header
-    
-    self[:question].each do |q|
-      msg.add_question(Question.new(q[:qname],q[:qtype],q[:qclass]))
-    end
-    
-    self[:answer].each do |rr|
-      msg.add_answer(RR.new_from_string(rr.to_rdata_string))
-    end
-    
-    self[:authority].each do |rr|
-      msg.add_authority(RR.new_from_string(rr.to_rdata_string))
-    end
-    
-    self[:additional].each do |rr|
-      msg.add_additional(RR.new_from_string(rr.to_rdata_string))
-    end
-    msg
-  end # to_dnsruby_message
-end # Hash
-
-# $LOAD_PATH.push(File.dirname(__FILE__))
+$LOAD_PATH.push(File.dirname(__FILE__))
+require 'lib/hash-to-dnsruby'
+require 'lib/simplecache'
+require 'lib/dnsruby-valid'
 
 # daemonize changes the directory to "/"
 Dir.chdir(File.dirname(__FILE__))
@@ -74,7 +28,7 @@ logfile = File.new(CONFIG[:log_file], 'a') # Always append
 $logger = Logging.logger(logfile)
 $logger.level = CONFIG[:log_level]
 
-class JSONDNSError
+class DnsrubyError
   def initialize(message = Message.new)
     @message = message
     @message.header.qr = true
@@ -88,32 +42,6 @@ class JSONDNSError
     rv = @message
     rv.header.rcode = 'SERVFAIL'
     rv.encode
-  end
-end
-
-class SimpleCache
-  @storage = nil
-  def initialize
-    @storage = {}
-  end
-  def set(key,value,expire_seconds=30)
-    @storage[key] = [Time.now + expire_seconds, value]
-  end
-  def get(key)
-    return nil unless @storage[key]
-    
-    if @storage[key][0] > Time.now
-      return @storage[key][1]
-    else
-      @storage.delete(key)
-    end
-    return nil
-  end
-end
-
-class Dnsruby::Message
-  def valid?
-    true
   end
 end
 
@@ -137,11 +65,11 @@ class JsonDns < EventMachine::Connection
       message = Dnsruby::Message.decode(data)
     rescue Exception => e
       $logger.error "Error decoding message: #{e.inspect}"
-      error = JSONDNSError.new
+      error = DnsrubyError.new
       return error.format_error
     end
 
-    error = JSONDNSError.new(message)
+    error = DnsrubyError.new(message)
 
     return error.format_error unless message.valid?
 
